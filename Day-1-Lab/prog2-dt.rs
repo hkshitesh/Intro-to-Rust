9455c7af8241010018ace6d560a6e707ce24d06c
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-10 23:05:18 +0530

    Data type added

---
+++ Day-1-Lab/prog2-dt.rs
@@ -0,0 +1,42 @@
+fn main()
+{
+    let x:u8 =255;
+    let res = 90;
+    let age:u32 =25;
+    let sum:i32 =10-15;
+    let m:isize = 20;
+    let n:usize= 30;
+    let f1 = 10.05; //f64 by default
+    let f2:f32 = 12.35;
+    let f3:f32 = 2555545555000.565;
+
+    let interest:f32 =100.0;
+    println!("x value is {}",x);
+    println!("res value is {}",res);
+    println!("age value is {}",age);
+    println!("sum value is {}",sum);
+    println!("rmes value is {}",m);
+    println!("n value is {}",n);
+    println!("f1 value is {}",f1);
+    println!("f2 value is {}",f2);
+    println!("f3 value is {}",f3);
+    println!("interest value is {}",interest);
+
+    //Boolean DT
+
+    let isfun:bool = true;
+    println!("Is Rust Programming Fun ? {}",isfun);
+
+    // CharacterDT
+    let special_character = '@'; //default
+    let alphabet:char = 'A';
+    let emoji:char = '😁';
+    
+    println!("special character is {}",special_character);
+    println!("alphabet is {}",alphabet);
+    println!("emoji is {}",emoji);
+ 
+     
+ 
+
+}
