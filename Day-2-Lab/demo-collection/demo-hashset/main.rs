d4d5e50071e7a3ccf009f5ffe601258f45918596
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-12 00:41:41 +0530

    HashSet and BTreeSet Added

---
+++ Day-2-Lab/demo-collection/demo-hashset/main.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+fn main()
+{
+let mut shapes = HashSet::new();
+shapes.insert("square");
+shapes.insert("circle");
+shapes.insert("triangle");
+shapes.insert("rectangle");
+shapes.insert("square");
+let has_circle = shapes.contains("hexagon");
+println!("Do this has circle is HashSet {}",has_circle);
+
+for shape in &shapes{
+    println!("Shape is {}",shape);
+}
+
+let rm_shape = shapes.remove("circle");
+println!("Deleted element is {:?}",rm_shape);
+for shape in &shapes{
+    println!("Shape is {}",shape);
+}
+
+}
