9be56ea78c718a5919d411900a80ee3a93c21e95
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-04-13 02:15:41 +0530

    code

---
+++ Day-3-Lab/demo-standard-lib-1/calc.rs
@@ -0,0 +1,16 @@
+pub fn sum(x:i32, y:i32)->i32
+{
+    return x+y;
+}
+pub fn diff(x:i32, y:i32)->i32
+{
+    return x-y;
+}
+pub fn mul(x:i32, y:i32)->i32
+{
+    return x*y;
+}
+pub fn div(x:i32, y:i32)->i32
+{
+    return x/y;
+}
---
+++ Day-3-Lab/demo-standard-lib-1/testcalc.rs
@@ -0,0 +1,8 @@
+fn main()
+{
+    let sum = calc::sum(20,10);
+    let diff = calc::diff(20,10);
+    let mul = calc::mul(20,10);
+    let div = calc::div(20,10);
+    println!("Sum is {} diff is {} mul is {} div is {}",sum,diff,mul,div);
+}
